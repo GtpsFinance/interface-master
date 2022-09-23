ea11aaeb12d410d7728c0c25a1e7884c239dbf48
GtpsFinance <welcome@gtps.finance>
2022-09-23 04:13:52 +0100

    Update AppBody.tsx

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -8,9 +8,9 @@ export const BodyWrapper = styled.div`
   width: 100%;
   background: ${({ theme }) => theme.bg1};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
-    0px 24px 32px rgba(0, 0, 0, 0.01);
+    0px 24px 32px rgba(29, 31, 36, 0.01);
   border-radius: 30px;
-  padding: 3rem;
+  padding: 6rem;
 `
 
 /**
